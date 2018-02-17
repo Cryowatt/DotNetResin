3fa8f8269786c135f3923713e8c4715e303d5a49
Eric Carter <eric.carter@cryowatt.com>
2018-02-16 21:41:19 -0800

    Updating autobuild script

--- Configure-Autobuilds.ps1
+++ Configure-Autobuilds.ps1
@@ -1,6 +1,7 @@
 [cmdletbinding()]
 param($Jwt)
 
+#$resinMachines = @('raspberry-pi')
 $resinMachines = @('raspberry-pi','raspberry-pi2','beaglebone-black','intel-edison','intel-nuc','via-vab820-quad',
     'zc702-zynq7','odroid-c1','odroid-xu4','parallella','nitrogen6x','hummingboard','ts4900','colibri-imx6dl',
     'apalis-imx6q','ts7700','raspberrypi3','artik5','artik10','beaglebone-green-wifi','qemux86','qemux86-64',
@@ -9,11 +10,15 @@ $resinMachines = @('raspberry-pi','raspberry-pi2','beaglebone-black','intel-edis
 
 $resinMachines | ForEach-Object {
     $ResinMachineName = $_
-    Invoke-RestMethod -Headers @{Authorization = "JWT $jwt"} -Method Delete https://hub.docker.com/v2/repositories/cryowatt/$ResinMachineName-dotnet-deps/
-    Invoke-RestMethod -Headers @{Authorization = "JWT $jwt"} -Method Delete https://hub.docker.com/v2/repositories/cryowatt/$ResinMachineName-dotnet/ 
-    $ImageSuffix = "dotnet-deps"
     $DockerFile = "deps.Dockerfile"
+    
+    $ImageSuffix = "dotnet-deps"
     $jobBody = $ExecutionContext.InvokeCommand.ExpandString((Get-Content .\AutomatedBuild.json | Out-String))
-    $jobBody
-    Invoke-RestMethod -Headers @{Authorization = "JWT $jwt"} -Method Post -ContentType "application/json" https://hub.docker.com/v2/repositories/cryowatt/raspberry-pi-dotnet/autobuild/ -Body $jobBody
+    #Invoke-RestMethod -Headers @{Authorization = "JWT $jwt"} -Method Delete https://hub.docker.com/v2/repositories/cryowatt/$ResinMachineName-$ImageSuffix/
+    Invoke-RestMethod -Headers @{Authorization = "JWT $jwt"} -Method Post -ContentType "application/json" https://hub.docker.com/v2/repositories/cryowatt/$ResinMachineName-$ImageSuffix/autobuild/ -Body $jobBody
+
+    $ImageSuffix = "dotnet"
+    $jobBody = $ExecutionContext.InvokeCommand.ExpandString((Get-Content .\AutomatedBuild.json | Out-String))
+    #Invoke-RestMethod -Headers @{Authorization = "JWT $jwt"} -Method Delete https://hub.docker.com/v2/repositories/cryowatt/$ResinMachineName-$ImageSuffix/
+    Invoke-RestMethod -Headers @{Authorization = "JWT $jwt"} -Method Post -ContentType "application/json" https://hub.docker.com/v2/repositories/cryowatt/$ResinMachineName-$ImageSuffix/autobuild/ -Body $jobBody
 }
