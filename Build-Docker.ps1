2114c8589da1f8245b1e7410f24e61bf47fea06c
Eric Carter <eric.carter@cryowatt.com>
2018-11-07 13:36:06 -0800

    Massive refactor of everything. Docker hub builds suck so I'm doing it myself.

---
+++ Build-Docker.ps1
@@ -0,0 +1,109 @@
+[cmdletbinding()]
+param(
+    [switch]$TagOnly=$false)
+
+Write-Progress -Activity "Building resin docker containers." -CurrentOperation "Downloading releases metadata." -Id 1
+$releases = Invoke-RestMethod https://raw.githubusercontent.com/dotnet/core/master/release-notes/releases.json
+$releases = $releases | Where-Object { $_.'runtime-linux-arm-x32' -ne $null }
+$releaseCount = 0
+$deviceName = "raspberry-pi2"
+$versions = @{}
+$versionPrefix = "cryowatt/resin:$deviceName-dotnet"
+
+& docker pull resin/${DEVICE_NAME}-debian:stretch
+
+foreach($release in $releases) {
+    $version = [System.Management.Automation.SemanticVersion]$release.'version-runtime'
+    $versionTag = "$versionPrefix-$version"
+    $downloadBlob = [uri]::new([uri]$release.'blob-runtime', $($release.'runtime-linux-arm-x32'))
+    $percentDone = (($releaseCount++ / $releases.Count) * 100)
+    Write-Progress -Activity "Building resin docker containers." -Status "$releaseCount/$($releases.Count) ($([int]$percentDone)%)" -Id 1 -PercentComplete $percentDone
+    
+    if (-not $TagOnly) {
+        Write-Progress -Activity "Building container for dotnet $versionTag" -CurrentOperation "Downloading checksums" -ParentId 1
+        $checksums = Invoke-RestMethod $release.'checksums-runtime'
+        if($checksums -match "(?<SHA>[a-f0-9]{128})\s+dotnet-runtime-.*?-linux-arm.tar.gz") {
+            $dotnetChecksum = $Matches["SHA"]
+        }
+        else{
+            throw "Checksum not found."
+        }
+
+        Write-Progress -Activity "Building container for dotnet $versionTag" -CurrentOperation "Building container" -ParentId 1
+
+        Write-Verbose DEVICE_NAME=$deviceName 
+        Write-Verbose DOTNET_VERSION=$version
+        Write-Verbose DOTNET_PACKAGE=$downloadBlob
+        Write-Verbose DOTNET_SHA512=$dotnetChecksum
+        & docker build `
+            --build-arg DEVICE_NAME=$deviceName `
+            --build-arg DOTNET_VERSION=$version `
+            --build-arg DOTNET_PACKAGE=$downloadBlob `
+            --build-arg DOTNET_SHA512=$dotnetChecksum `
+            --tag $versionTag `
+            . | Write-Verbose
+
+        if (-not $?) {
+            throw "Failed to build container $versionTag"
+        }
+    }
+
+    $versions.Add($version, $versionTag)
+}
+
+### Tag latest release majors
+Write-Progress -Activity "Tagging major versions" -ParentId 1
+
+$versions.Keys | Group-Object -Property Major | ForEach-Object {
+    $topVersion = $_.Group | Where-Object { $_.PreReleaseLabel -eq $null } | Sort-Object -Descending | Select-Object -First 1
+
+    if($topVersion -eq $null) {
+        $topVersion = $_.Group | Sort-Object -Descending | Select-Object -First 1      
+    }
+    
+    Write-Verbose "Tagging $($versions[$topVersion]) <- $versionPrefix-$($topVersion.Major)"
+    docker tag $($versions[$topVersion]) $versionPrefix-$($topVersion.Major) | Write-Verbose
+    
+    if (-not $?) {
+        throw "Tagging failed $($versions[$topVersion]) <- $versionPrefix-$($topVersion.Major)"
+    }
+}
+
+### Tag latest release minors
+Write-Progress -Activity "Tagging minor versions" -ParentId 1
+
+$versions.Keys | Group-Object -Property Major, Minor | ForEach-Object {
+    $topVersion = $_.Group | Where-Object { $_.PreReleaseLabel -eq $null } | Sort-Object -Descending | Select-Object -First 1
+
+    if($topVersion -eq $null) {
+        $topVersion = $_.Group | Sort-Object -Descending | Select-Object -First 1      
+    }
+    
+    Write-Verbose "Tagging $($versions[$topVersion]) <- $versionPrefix-$($topVersion.Major).$($topVersion.Minor)"
+    docker tag $versions[$topVersion] $versionPrefix-$($topVersion.Major).$($topVersion.Minor)
+    
+    if (-not $?) {
+        throw "Tagging failed $($versions[$topVersion]) <- $versionPrefix-$($topVersion.Major).$($topVersion.Minor)"
+    }
+}
+
+### Tag latest release
+Write-Progress -Activity "Tagging latest version" -ParentId 1
+
+$topVersion = $versions.Keys | Where-Object { $_.PreReleaseLabel -eq $null } | Sort-Object -Descending | Select-Object -First 1
+if($topVersion -eq $null) {
+    $topVersion = $_.Group | Sort-Object -Descending | Select-Object -First 1      
+}
+
+Write-Verbose "Tagging $($versions[$topVersion]) <- $versionPrefix"
+docker tag $versions[$topVersion] $versionPrefix
+
+if (-not $?) {
+    throw "Tagging failed $($versions[$topVersion]) <- $versionPrefix"
+}
+
+$i = 0
+$versions.Values | ForEach-Object {
+    Write-Progress -Activity "Pushing images" -CurrentOperation "Pushing $_" -PercentComplete (($i++ / $versions.Count) * 100) -ParentId 1
+    & docker push $_
+}
---
+++ Dockerfile
@@ -0,0 +1,48 @@
+#### RUNTIME DEPS ####
+ARG DEVICE_NAME
+FROM resin/${DEVICE_NAME}-debian:stretch AS runtime-deps
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        ca-certificates \   
+        \
+# .NET Core dependencies
+        libc6 \
+        libgcc1 \
+        libgssapi-krb5-2 \
+        libicu57 \
+        liblttng-ust0 \
+        libssl1.0.2 \
+        libstdc++6 \
+        zlib1g \
+    && rm -rf /var/lib/apt/lists/*
+
+# Configure Kestrel web server to bind to port 80 when present
+ENV ASPNETCORE_URLS=http://+:80 \
+    # Enable detection of running in a container
+    DOTNET_RUNNING_IN_CONTAINER=true
+
+
+
+#### RUNTIME ####
+FROM runtime-deps AS runtime
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        curl \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install .NET Core
+ARG DOTNET_VERSION
+ARG DOTNET_PACKAGE
+ARG DOTNET_SHA512
+ENV DOTNET_VERSION ${DOTNET_VERSION}
+ENV DOTNET_PACKAGE ${DOTNET_PACKAGE}
+ENV DOTNET_SHA512 ${DOTNET_SHA512}
+
+RUN curl -SL --output dotnet.tar.gz ${DOTNET_PACKAGE} \
+    && echo "${DOTNET_SHA512} dotnet.tar.gz" | sha512sum -c - \
+    && mkdir -p /usr/share/dotnet \
+    && tar -zxf dotnet.tar.gz -C /usr/share/dotnet \
+    && rm dotnet.tar.gz \
+    && ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
